f3b1ed64c10d2927f248dcbc6198ae8aa64a6632
lordneogen <123793129+lordneogen@users.noreply.github.com>
2023-08-21 07:48:36 +0600

    fix:invers

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont
 from math import ceil
 
+    
 def add_watermark(input_image_path, output_image_path, watermark_text):
     original_image = Image.open(input_image_path)
     width, height = original_image.size
     
     watermark = Image.new("RGBA", original_image.size)
     draw = ImageDraw.Draw(watermark, "RGBA")
-    
+
+    text = watermark_text
     font_size=ceil(height/width*min(75,int(width/len(watermark_text))))
     print(height,width,font_size)
-    text = watermark_text
-    font = ImageFont.truetype("Mexaronzy.ttf", font_size)
-
-    text_width, text_height = draw.textsize(text, font)
+    
+    font = ImageFont.truetype("Mexaronzy.ttf", font_size)  # Замените на путь к вашему шрифту
+    
+    print(draw.textlength(text, font=font))
+    text_width = int(draw.textlength(text, font=font))
+    text_height=font_size
     x_pos = width - text_width - 10
     y_pos = height - text_height - 10
 
-    draw.text((x_pos, y_pos), text, font=font, fill=(255, 255, 255, 128))
+    draw.rectangle([(x_pos, y_pos), (x_pos + text_width, y_pos + text_height)], fill=(0, 0, 0, 128))
+    draw.text((x_pos, y_pos), text, font=font, fill=(255, 255, 255, 255))
     
     watermarked_image = Image.alpha_composite(original_image.convert("RGBA"), watermark)
-    watermarked_image.save(output_image_path, "PNG")
-    
+    watermarked_image.save(output_image_path, "PNG")    
+
+
 import sys
 
 
